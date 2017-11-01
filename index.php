0550ad305224e2bedc57ca33d4a89c155440d9a2
Unknown <di118@live.mdx.ac.uk>
2017-11-01 18:02:14 +0000

    Fixed registration(only valid emails + matching passwords)
    -_-

--- index.php
+++ index.php
@@ -1,32 +1,26 @@
 <?php
-    //Include the PHP functions to be used on the page 
-    include('header1.php'); 
-	
-    /<?php
-    //Include the PHP functions to be used on the page 
-    include('header1.php'); 
-	
-    //Output header and navigation 
-    outputHeader("The Road Layer - Game - Home");
-    outputBannerNavigation("Home");
-?>
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand " href="#" >THE ROAD LAYER</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="leaderboard.html">Leaderboard</a></li>
+                    <li><a href="login.html">Login/Logout</a></li>
+                    <li><a href="register.html">Register</a></li>
+                </ul>
 
-<!-- Contents of the page -->
-<h1>This is the home page of the game</h1>
+            </div><!--/.nav-collapse -->
+        </div>
+    </nav>
 
-<?php
-    //Output the footer
-    outputFooter();
-?>/Output header and navigation 
-    outputHeader("The Road Layer - Game - Home);
-    outputBannerNavigation("Home");
-?>
 
-<!-- Contents of the page -->
-<h1>This is the home page of the game</h1>
-
-<?php
-    //Output the footer
-    outputFooter();
 ?> 
-<!- LET's check for GIT ->
--- js/danielScripts.js
+++ js/danielScripts.js
@@ -1,68 +1,85 @@
 
-	function saveRegisterDetails(){
-		var userDetails = {};
-		userDetails.firstName = document.getElementById("firstNameRegister").value;
-		userDetails.lastNameRegister = document.getElementById("lastNameRegister").value;
-		userDetails.emailRegister = document.getElementById("emailRegister").value;
-		userDetails.passwordRegister = document.getElementById("passwordRegister").value;
-		userDetails.confirmPasswordRegister = document.getElementById("confirmPasswordRegister").value;
-		localStorage[userDetails.emailRegister] = JSON.stringify(userDetails);
-		document.getElementById("registrationOutcome").innerHTML = " You have registered successfully. <br> Redirecting to login page .....";
-
-		setTimeout(function () {
-       			window.location.href = "login.html"; //will redirect to  login page 
-    			}, 3000); //will call the function after 3 secs.
-
-	}	
-
-	function seeIfLogged(){
-		if(localStorage.loggedInUserEmail !== undefined)
+function saveRegisterDetails(){
+	var userDetails = {};
+	userDetails.firstName = document.getElementById("firstNameRegister").value;
+	userDetails.lastNameRegister = document.getElementById("lastNameRegister").value;
+	userDetails.emailRegister = document.getElementById("emailRegister").value;
+	userDetails.passwordRegister = document.getElementById("passwordRegister").value;
+	userDetails.confirmPasswordRegister = document.getElementById("confirmPasswordRegister").value;
+	if(emailRegister.checkValidity())
+	{	
+		if(userDetails.confirmPasswordRegister == userDetails.passwordRegister)		
 		{
-			var userDetails = JSON.parse(localStorage[localStorage.loggedInUserEmail])	
-			document.getElementById("container11").remove();
-			document.getElementById("loginOutcome2").innerHTML=userDetails.lastNameRegister + " logged in.";
-			document.getElementById("logoutButton").innerText= "Logout";
-
-
-
-
-		}
-
-	}
-
-	function login(){
-		var email = document.getElementById("emailLogin").value;
-		if(localStorage[email] === undefined)
-		{
-			document.getElementById("loginOutcome").innerHTML = "Email not found. Please create an account.";
-			return;
+			localStorage[userDetails.emailRegister] = JSON.stringify(userDetails);
+			document.getElementById("registrationOutcome").innerHTML = " You have registered successfully. <br> Redirecting to login page .....";
+			setTimeout(function () {
+		       			window.location.href = "login.html"; //will redirect to  login page 
+		    			}, 3000); //will call the function after 3 seconds
 		}
 		else
 		{
-			var userDetails = JSON.parse(localStorage[email])
-			var password = document.getElementById("passwordLogin").value;
-			if(password === userDetails.passwordRegister)
-				{
-					document.getElementById("loginOutcome2").innerHTML=userDetails.lastNameRegister + " logged in. <br> Redirecting to the game page .....";
-					document.getElementById("loginOutcome").innerHTML=" ";
-					localStorage.loggedInUserEmail = userDetails.emailRegister;
+			
+			document.getElementById("registrationOutcome").innerHTML = " Password doesn't match";
+			userDetails.passwordRegister = document.getElementById("passwordRegister").value;
+			userDetails.confirmPasswordRegister = document.getElementById("confirmPasswordRegister").value;
+		}
+	}
+	else
+		document.getElementById("registrationOutcome").innerHTML = " Invalid inputs";
+	
+	
 
-					setTimeout(function () {
+}	
+
+function seeIfLogged(){
+	if(localStorage.loggedInUserEmail !== undefined)
+	{
+		var userDetails = JSON.parse(localStorage[localStorage.loggedInUserEmail])	
+		document.getElementById("container11").remove();
+		document.getElementById("loginOutcome2").innerHTML=userDetails.lastNameRegister + " logged in.";
+		document.getElementById("logoutButton").innerText= "Logout";
+
+
+
+
+	}
+
+}
+
+function login(){
+	var email = document.getElementById("emailLogin").value;
+	if(localStorage[email] === undefined)
+	{
+		document.getElementById("loginOutcome").innerHTML = "Email not found. Please create an account.";
+		return;
+	}
+	else
+	{
+		var userDetails = JSON.parse(localStorage[email])
+		var password = document.getElementById("passwordLogin").value;
+		if(password === userDetails.passwordRegister)
+		{
+			document.getElementById("loginOutcome2").innerHTML=userDetails.lastNameRegister + " logged in. <br> Redirecting to the game page .....";
+			document.getElementById("loginOutcome").innerHTML=" ";
+			localStorage.loggedInUserEmail = userDetails.emailRegister;
+
+			setTimeout(function () {
 		       			window.location.href = "index.html"; //will redirect to  login page 
-		    			}, 3000); //will call the function after 3 secs.
-
-				}
-			else
-					document.getElementById("loginOutcome").innerHTML="Email or password not recognized."
+		    			}, 3000); //will call the function after 3 seconds.
 
 		}
-		
+		else
+			document.getElementById("loginOutcome").innerHTML="Email or password not recognized."
+
+	}
+	
 
 
 
 }
-	function logout()
-	{
-		localStorage.loggedInUserEmail = undefined;
-		location.reload();
-	}
+function logout()
+{
+	localStorage.loggedInUserEmail = undefined;
+	location.reload();
+}
+
