15bbdf21f1d7458a1f7537b3244e3b504a8ad7d6
Unknown <di118@live.mdx.ac.uk>
2017-11-06 00:34:02 +0000

    removed "aaaaa" - test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 ?>
 
 <!-- Contents of the page -->
-<body>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+<body>
     <div class ="container">
             <canvas id="myCanvas" width="1000" height="800" style="border:1px solid #000000;"></canvas>
     </div>
